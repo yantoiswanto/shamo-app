e7fddc7849a6bc9ee644aa3df54027c8123ed2a0
Yanto Iswanto <yantoiswanto18@gmail.com>
2023-01-23 16:51:32 +0700

    Slicing Home Page Popular Products

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shamo_app/theme.dart';
+import 'package:shamo_app/widget/product_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -167,10 +168,49 @@ class HomePage extends StatelessWidget {
       );
     }
 
+    Widget popularProductTitle() {
+      return Container(
+        margin: EdgeInsets.only(
+            top: defaultMargin, left: defaultMargin, right: defaultMargin),
+        child: Text(
+          'Popular Products',
+          style: primaryTextStyle.copyWith(
+            fontSize: 22,
+            fontWeight: semiBold,
+          ),
+        ),
+      );
+    }
+
+    Widget popularProducts() {
+      return Container(
+        margin: EdgeInsets.only(top: 14),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              SizedBox(
+                width: defaultMargin,
+              ),
+              Row(
+                children: [
+                  ProductCard(),
+                  ProductCard(),
+                  ProductCard(),
+                ],
+              )
+            ],
+          ),
+        ),
+      );
+    }
+
     return ListView(
       children: [
         header(),
         categories(),
+        popularProductTitle(),
+        popularProducts(),
       ],
     );
   }
