32b7a5596a48ce27dec41eb37d24cbca94f9be76
Yanto Iswanto <yantoiswanto18@gmail.com>
2023-01-23 18:07:57 +0700

    Slicing Detail Chat Page Header

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shamo_app/pages/detail_chat_page.dart';
 import 'package:shamo_app/pages/home/main_page.dart';
 import 'package:shamo_app/pages/sign_in_page.dart';
 import 'package:shamo_app/pages/sign_up_page.dart';
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
         '/sign-in': (context) => SignInPage(),
         '/sign-up': (context) => SignUpPage(),
         '/home': (context) => MainPage(),
+        '/detail-chat': (context) => DetailChatPage(),
       },
     );
   }
