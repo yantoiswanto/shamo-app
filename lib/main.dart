d8581cff3fc7d241669510b3992a16d60d3e80cf
Yanto Iswanto <yantoiswanto18@gmail.com>
2023-01-24 07:44:16 +0700

    slicing cart page, snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shamo_app/pages/cart_page.dart';
 import 'package:shamo_app/pages/detail_chat_page.dart';
 import 'package:shamo_app/pages/edit_profile_page.dart';
 import 'package:shamo_app/pages/home/main_page.dart';
@@ -27,6 +28,7 @@ class MyApp extends StatelessWidget {
         '/detail-chat': (context) => DetailChatPage(),
         '/edit-profile': (context) => EditProfilePage(),
         '/product': (context) => ProoductPage(),
+        '/cart': (context) => ChartPage(),
       },
     );
   }
--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -15,8 +15,96 @@ class _ProoductPageState extends State<ProoductPage> {
     'assets/image_shoes.png',
     'assets/image_shoes.png'
   ];
+  List familiarShoes = [
+    'assets/image_shoes.png',
+    'assets/image_shoes.png',
+    'assets/image_shoes.png',
+    'assets/image_shoes.png',
+    'assets/image_shoes.png',
+    'assets/image_shoes.png',
+    'assets/image_shoes.png',
+    'assets/image_shoes.png',
+    'assets/image_shoes.png'
+  ];
 
   int currentIndex = 0;
+  bool isWishlist = false;
+
+  Future<void> showSuccessDialog() async {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) => Container(
+        width: MediaQuery.of(context).size.width - (2 * defaultMargin),
+        child: AlertDialog(
+          backgroundColor: backgroundColor3,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(
+                      Icons.close,
+                      color: primaryTextColor,
+                    ),
+                  ),
+                ),
+                Image.asset(
+                  'assets/icon_success.png',
+                  width: 100,
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  'Hurray :)',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 18,
+                    fontWeight: semiBold,
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  'Item added successfully',
+                  style: secondaryTextStyle,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: 154,
+                  height: 44,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        backgroundColor: primaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        )),
+                    child: Text(
+                      'View My Cart',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   Widget indicator(int index) {
     return Container(
@@ -28,6 +116,24 @@ class _ProoductPageState extends State<ProoductPage> {
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
         color: currentIndex == index ? primaryColor : Color(0xffC4C4C4),
+      ),
+    );
+  }
+
+  Widget familiarShoesCard(String imageUrl) {
+    return Container(
+      width: 54,
+      height: 54,
+      margin: EdgeInsets.only(
+        right: 16,
+      ),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            imageUrl,
+          ),
+        ),
+        borderRadius: BorderRadius.circular(6),
       ),
     );
   }
@@ -92,6 +198,241 @@ class _ProoductPageState extends State<ProoductPage> {
     );
   }
 
+  Widget content() {
+    int index = -1;
+    return Container(
+      margin: EdgeInsets.only(top: 17),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(24),
+        ),
+        color: backgroundColor1,
+      ),
+      child: Column(
+        children: [
+          // NOTE: HEADER
+          Container(
+            margin: EdgeInsets.only(
+                top: defaultMargin, left: defaultMargin, right: defaultMargin),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'TERREX URBAN LOW',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 18,
+                          fontWeight: semiBold,
+                        ),
+                      ),
+                      Text(
+                        'Hiking',
+                        style: secondaryTextStyle.copyWith(
+                          fontSize: 12,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isWishlist = !isWishlist;
+                    });
+                    if (isWishlist) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          backgroundColor: secondaryColor,
+                          content: Text(
+                            'Has been added to the Wistlist',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          backgroundColor: alertColor,
+                          content: Text(
+                            'Has been removed from the Wistlist',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  child: Image.asset(
+                    isWishlist
+                        ? 'assets/button_wishlist_blue.png'
+                        : 'assets/button_wishlist.png',
+                    width: 46,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // NOTE: PRICE
+          Container(
+            margin: EdgeInsets.only(
+              top: 20,
+              left: defaultMargin,
+              right: defaultMargin,
+            ),
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: backgroundColor2,
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Price starts from',
+                  style: primaryTextStyle,
+                ),
+                Text(
+                  '\$143,98',
+                  style: priceTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: semiBold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          //NOTE: DESCRIPTION
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(
+              top: defaultMargin,
+              right: defaultMargin,
+              left: defaultMargin,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Description',
+                  style: primaryTextStyle.copyWith(
+                    fontWeight: medium,
+                  ),
+                ),
+                SizedBox(
+                  width: 12,
+                ),
+                Text(
+                  'Unpaved trails and mixed surfaces are easy when you have the traction and support you need. Casual enough for the daily commute.',
+                  style: subtitleTextStyle.copyWith(
+                    fontWeight: light,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+              ],
+            ),
+          ),
+
+          //NOTE: FAMILIAR SHOES
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(top: defaultMargin),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: defaultMargin,
+                  ),
+                  child: Text(
+                    'Fimiliar Shoes',
+                    style: primaryTextStyle.copyWith(
+                      fontWeight: medium,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: familiarShoes.map(
+                      (image) {
+                        index++;
+                        return Container(
+                          margin: EdgeInsets.only(
+                              left: index == 0 ? defaultMargin : 0),
+                          child: familiarShoesCard(image),
+                        );
+                      },
+                    ).toList(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // NOTE: BUTTONS
+
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.all(defaultMargin),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/detail-chat');
+                  },
+                  child: Container(
+                    width: 54,
+                    height: 54,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/button_chat.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Container(
+                    height: 54,
+                    child: TextButton(
+                      onPressed: (() {
+                        showSuccessDialog();
+                      }),
+                      style: TextButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        backgroundColor: primaryColor,
+                      ),
+                      child: Text(
+                        'add to Cart',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 16,
+                          fontWeight: semiBold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -99,6 +440,7 @@ class _ProoductPageState extends State<ProoductPage> {
       body: ListView(
         children: [
           header(),
+          content(),
         ],
       ),
     );
